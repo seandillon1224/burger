d1437cf7b239e9033cdfd6429bfbc89f2021373a
Sean Dillon <seandillon1224@gmail.com>
2018-02-16 13:34:39 -0500

    newadds

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+// mysql://b0eee3b726d6bc:d470811d@us-cdbr-iron-east-05.cleardb.net/heroku_d838a86c021266c?reconnect=true
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burger_db"
+// });
+
 var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burger_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b0eee3b726d6bc",
+  password: "d470811d",
+  database: "heroku_d838a86c021266c"
 });
 
 // Make connection.
